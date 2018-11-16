28c95ca62403ad1568596431a049824e9d2a05d5
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-11-16 13:30:18 +0100

    contactsheet for mobile done

--- leaveemail.php
+++ leaveemail.php
@@ -10,8 +10,12 @@ require __DIR__.'/header.php';
     <img src="./images/contactsheet-background.png" alt="">
   </div>
 
+  <button class="cross-button" type="button" name="button">
+    <i class="fas fa-times fa-3x"></i>
+  </button>
+
   <div class="contactsheet">
-    <h2>create your <br>special <br>adventures.</h2>
+    <h2>create your <br>unique <br>adventures.</h2>
     <img class="saab-big" src="SAAB_fakelogo.png" alt="Logo">
     <img class="fs-logo" src="./images/FASTSECURITY_FS_LOGO.png" alt="Logo">
     <p>To recieve more info about FastSecurity™<br />
