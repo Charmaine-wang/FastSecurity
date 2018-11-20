2597afbbfd9ab8bc216a083d5f6681d2066d3f5e
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-11-20 15:38:24 +0100

    fixed back button on confirmation page

--- confirmation.php
+++ confirmation.php
@@ -13,12 +13,11 @@ require __DIR__.'/header.php';
   <div class="confirmation">
     <img class="saab-big" src="SAAB_fakelogo.png" alt="Logo">
     <img class="fs-logo2" src="./images/FASTSECURITY_FS_LOGO.png" alt="Logo">
-    <p class="thank-you">Thank you for<br>registrating!</p>
-    <p class="learn-more">continue learning more about <br>fast security and download our <br>brochure below</p>
-    <form class="contact-form" action="" method="post">
-     	 <button class="download-button" type="submit">Download your brochure here <i class="fas fa-download"></i></button>
-    </form>
+    <h2>Thank you for <br>registrating!</h2>
+    <p>continue learning more about <br>fast security and download our <br>brochure below</p>
+    <button class="download-button" type="submit">Download your brochure here <i class="fas fa-download"></i></button>
     <hr class="hr-download">
+    <a href="#"><button class="back-button" type="button" name="button"><i class="fas fa-angle-left"></i> Take me back</button></a>
   </div>
 
 </section>
