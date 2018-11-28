bca62010874327712cffe2724e777f96b1bfa256
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-11-28 14:32:11 +0100

    made broschure page

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
   <div class="saablogo-footer">
-    <img  class="logo-footer" src="images/FASTSECURITY_FS_logo.png" alt="">
     <img class="saab-footer" src="images/saablogo.png" alt="">
+    <img  class="logo-footer" src="images/FASTSECURITY_FS_logo.png" alt="">
   <a href="contact.php"><button type="button" class="footer-button">Start your journey</button></a>
   </div>
 
@@ -15,10 +15,14 @@
     <ul>
       <li class="only-desktop">Copyright SAAB AB</li>
       <li>Legal Notice
-        <i class="far fa-share-square"></i>
+        <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M1 0.5H0.5V1V21V21.5H1H23H23.5V21V1V0.5H23H1ZM1.5 20.5V1.5H22.5V20.5H1.5ZM20.8176 3.92102C20.8176 3.46915 20.4194 3.10284 19.9282 3.10284H11.9239C11.4328 3.10284 11.0346 3.46915 11.0346 3.92102C11.0346 4.37288 11.4328 4.7392 11.9239 4.7392H17.7814L3.34901 18.0164L4.60676 19.1735L19.0389 5.89651V11.2846C19.0389 11.7365 19.437 12.1028 19.9282 12.1028C20.4194 12.1028 20.8176 11.7365 20.8176 11.2846V3.92102Z" fill="#BEA683"/>
+        </svg>
       </li>
       <li>Glossary
-        <i class="far fa-share-square"></i>
+        <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M1 0.5H0.5V1V21V21.5H1H23H23.5V21V1V0.5H23H1ZM1.5 20.5V1.5H22.5V20.5H1.5ZM20.8176 3.92102C20.8176 3.46915 20.4194 3.10284 19.9282 3.10284H11.9239C11.4328 3.10284 11.0346 3.46915 11.0346 3.92102C11.0346 4.37288 11.4328 4.7392 11.9239 4.7392H17.7814L3.34901 18.0164L4.60676 19.1735L19.0389 5.89651V11.2846C19.0389 11.7365 19.437 12.1028 19.9282 12.1028C20.4194 12.1028 20.8176 11.7365 20.8176 11.2846V3.92102Z" fill="#BEA683"/>
+        </svg>
       </li>
     </ul>
   </div>
